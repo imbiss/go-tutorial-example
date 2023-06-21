8bc4117655f62e35680c4a9c738cc92387a473d1
Hongyi Chen <Hongyi.Chen@mail.schwarz>
2023-06-21 12:22:14 +0200

    fix syntax

--- greetings/greeting.go
+++ greetings/greeting.go
@@ -3,6 +3,6 @@ package greetings
 import "fmt"
 
 func Hello(name string) string {
-   message :-= fmt.Sprintf("Hi %v, welcome", name)
+   message := fmt.Sprintf("Hi %v, welcome", name)
    return message
 }
